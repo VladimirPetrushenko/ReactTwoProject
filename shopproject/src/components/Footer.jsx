3bdd27ec84e9f3f376ffe84f8715dd4c72ce4317
Vladimir Petrushenko <barrierofdust@gmail.com>
2021-03-29 11:49:14 +0300

    Add Readme.MD

--- shopproject/src/components/Footer.jsx
+++ shopproject/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export function Footer(){
     return <footer className="page-footer light-green lighten-3">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Shop Fortnite</a>
+      <a className="grey-text text-lighten-4 right" href="https://fortniteapi.io/">Fortnite api</a>
       </div>
   </footer>
 }
--- shopproject/src/components/Header.jsx
+++ shopproject/src/components/Header.jsx
@@ -1,9 +1,9 @@
 export function Header(){
     return <nav className="light-green lighten-1">
     <div className="nav-wrapper" >
-      <a href="!#" className="brand-logo">React Shop Fortnite</a>
+      <a href="/" className="brand-logo">React Shop Fortnite</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="!#">Repository</a></li>
+        <li><a href="https://github.com/VladimirPetrushenko/ReactTwoProject/tree/main/shopproject">Repository</a></li>
       </ul>
     </div>
   </nav>
